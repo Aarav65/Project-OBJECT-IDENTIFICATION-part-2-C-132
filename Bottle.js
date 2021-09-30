d6fc3b082fa13d7b70dc5bac595dc2da0abc251f
Aarav65 <76677576+Aarav65@users.noreply.github.com>
2021-09-30 17:46:19 +0530

    Add files via upload

---
+++ Bottle.js
@@ -0,0 +1,22 @@
+img = "";
+
+function  preload()
+{
+    img = loadImage('bottle.jpg');
+}
+
+function setup()
+{
+    canvas = createCanvas(600, 400);
+    canvas.center();
+}
+
+function draw()
+{
+    image(img, 0, 0, 600, 400); 
+}
+
+function back()
+{
+    window.location = "home.html";
+}
---
+++ home.js
@@ -0,0 +1,24 @@
+function dogCat()
+{
+    window.location = "dogcat.html";
+}
+
+function bedroom()
+{
+    window.location = "bedroom.html";
+}
+
+function desk()
+{
+    window.location = "desk.html";
+}
+
+function bottle()
+{
+    window.location = "bottle.html";
+}
+
+function fruitBasket()
+{
+    window.location = "fruitbasket.html";
+}
